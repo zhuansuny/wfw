de27793b2dbb480d425a2b88ab0ff83e5d20a1b8
zsyy <864919639@qq.com>
2021-07-10 09:31:58 +0800

    fit package

--- trie.go
+++ trie.go
@@ -5,14 +5,15 @@ import (
 )
 
 type node struct {
-	pattern  string  // 待匹配路由，例如 /p/:lang
+	pattern  string  // 待匹配路由，例如 /api/:lang
 	part     string  // 路由中的一部分，例如 :lang
-	children []*node // 子节点，例如 [doc, tutorial, intro]
+	children []*node // 子节点，
 	isWild   bool    // 是否精确匹配，part 含有 : 或 * 时为true
 }
 
 // 第一个匹配成功的节点，用于插入
 func (n *node) matchChild(part string) *node {
+
 	for _, child := range n.children {
 		if child.part == part || child.isWild {
 			return child
--- validator.go
+++ validator.go
@@ -1,4 +1,4 @@
-package webfw
+package wfw
 
 import (
 	"errors"
